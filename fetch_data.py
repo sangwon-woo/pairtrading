d5d1528e6018ae7660ee6b60733400677883812e
sangwon-woo <riddlewoo713@gmail.com>
2024-05-27 19:46:53 +0900

    temporary

--- fetch_data.py
+++ fetch_data.py
@@ -1,13 +1,4 @@
-import configparser
-
-parser = configparser.ConfigParser()
-parser.read('config/config.ini')
-
-UPBIT_ACCESS_KEY = parser.get('upbit', 'access_key')
-UPBIT_SECRET_KEY = parser.get('upbit', 'secret_key')
-DIR_UPBIT_DATABASE = 'D:\\coin_database\\domestic\\upbit'
-DIR_UPBIT_DAILY_CANDLE = DIR_UPBIT_DATABASE + '\\daily'
-DIR_UPBIT_MINUTELY_CANDLE = DIR_UPBIT_DATABASE + '\\minutely'
-DIR_UPBIT_DATABASE_BACKUP = 'E:\\backup\\coin_database\\domestic\\upbit'
-DIR_UPBIT_DAILY_CANDLE_BACKUP = DIR_UPBIT_DATABASE_BACKUP + '\\daily'
-DIR_UPBIT_MINUTELY_CANDLE_BACKUP = DIR_UPBIT_DATABASE_BACKUP + '\\minutely'
+import os
+import datetime
+import time
+from config.setting import *
